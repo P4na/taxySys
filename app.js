91300732911c2b40686b14eb574ae037fa25e281
P4na <Vip_P4na@protonmail.com>
2022-10-18 11:32:06 +0200

    capire come inserire il post e inviare i dati al pg

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
+const { Router } = require("express")
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
+
 const app = express()
 
-publicPath = {root: __dirname + "/public"}
 
 //ejs layouts
+app.use(expressLayouts)
 app.set("view engine","ejs")
 // app.use(expressLayouts)
 
-
 app.get("/", (req, res)=>{
-    res.render("index")
+    res.render("home", {title:"home"})
 })
 
-app.get("/signup", (req, res)=>{
-    res.sendFile("signup.html", publicPath)
+ app.get("/signup", (req, res)=>{
+    res.render("signup", {title:"sign up"})
 })
 
 app.get("/signin", (req, res)=>{
-    res.sendFile("signin.html", publicPath)
+    res.render("signin", {title:"sign in"})
+}) 
+
+app.post("/signinpost", (req, res)=>{
+    console.log(req.body)
+    res.sendStatus(200)
 })
+
+app.get("/trypost", (req, res)=>{
+    res.status(200)
+    .render('trypost', {title:"trypost"})
+    console.log(req.body)
+})
+
+
 
 app.listen(3000)
